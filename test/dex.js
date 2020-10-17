75b10854445f5ce482ecb3943825a380df8c289a
Benjamin M. Brown <benjaminbb@gmail.com>
2020-10-17 00:48:26 +0000

    adding test setup

--- test/dex.js
+++ test/dex.js
@@ -1,17 +1,29 @@
-const DAI = artifacts.require('mocks/MockDAI.sol');
-const BAT = artifacts.require('mocks/MockBAT.sol');
-const YFI = artifacts.require('mocks/MockYFI.sol');
-const CWV = artifacts.require('mocks/ChainwaveToken.sol');
+const Dai = artifacts.require('mocks/MockDAI.sol');
+const Bat = artifacts.require('mocks/MockBAT.sol');
+const Yfi = artifacts.require('mocks/MockYFI.sol');
+const Cwv = artifacts.require('mocks/ChainwaveToken.sol');
+
+const DEX = artifacts.require('ChainwaveDex.sol');
 
 contract ('ChainwaveDex', () => {
     let dai,bat,yfi,cwv;
+    const [DAI, BAT, YFI, CWV] = ['DAI', 'BAT', 'YFI', 'CWV'].map(ticker => web3.utils.fromAscii(ticker))
     beforeEach(async()=>{
-        
         ([dai,bat,yfi,cwv] = await Promise.all([
-            DAI.new(),
-            BAT.new(),
-            YFI.new(),
-            CWV.new()
+            Dai.new(),
+            Bat.new(),
+            Yfi.new(),
+            Cwv.new()
         ]));
+
+        const dex = await DEX.new();
+
+        await Promise.all([
+            dex.addToken(DAI, dai.address),
+            dex.addToken(BAT, bat.address),
+            dex.addToken(YFI, yfi.address),
+            dex.addToken(CWV, cwv.address),
+        ])
+
     })
 })
