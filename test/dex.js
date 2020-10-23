2c3c456f2ee57fb78ce33eb84ad29254c34ff9b9
Benjamin M. Brown <benjaminbb@gmail.com>
2020-10-23 03:57:44 +0000

    starting testing for order types

--- test/dex.js
+++ test/dex.js
@@ -1,5 +1,5 @@
 const { assert } = require("console");
-const { expectRevert} = require('@openzeppelin/test-helpers');
+const { expectRevert } = require('@openzeppelin/test-helpers');
 const { expect } = require("chai");
 const { web3 } = require("@openzeppelin/test-helpers/src/setup");
 const Dai = artifacts.require('mocks/MockDAI.sol');
@@ -7,6 +7,11 @@ const Bat = artifacts.require('mocks/MockBAT.sol');
 const Yfi = artifacts.require('mocks/MockYFI.sol');
 const Cwv = artifacts.require('mocks/ChainwaveToken.sol');
 const DEX = artifacts.require('ChainwaveDex.sol');
+
+const SIDE = {
+    BUY: 0,
+    SELL: 1
+}
 
 contract('ChainwaveDex', (accounts) => {
 
@@ -43,75 +48,99 @@ contract('ChainwaveDex', (accounts) => {
 
         await Promise.all([
             [dai, bat, yfi, cwv]
-            .map(
-                token => seedTokenBalance(token, trader1)
-            )
+                .map(
+                    token => seedTokenBalance(token, trader1)
+                )
         ])
 
         await Promise.all([
             [dai, bat, yfi, cwv]
-            .map(
-                token => seedTokenBalance(token, trader2)
-            )
+                .map(
+                    token => seedTokenBalance(token, trader2)
+                )
         ])
     })
 
-    it('Should deposit tokens', async()=>{
-      
+    it('Should deposit tokens', async () => {
+
         const amount = web3.utils.toWei('100');
-        await dex.deposit(amount, DAI, {from:trader1})
-        const balance = await dex.traderBalances(trader1,DAI);
+        await dex.deposit(amount, DAI, { from: trader1 })
+        const balance = await dex.traderBalances(trader1, DAI);
         console.log("bal aft", balance.toString());
         assert(balance.toString() === amount);
     })
 
-    it('Should Not deposit tokens if token doesnt exist', async()=>{
-     await expectRevert(
-         dex.deposit(
-             web3.utils.toWei('100'),
-             web3.utils.fromAscii('FAKE TOKEN'), 
-             {from: trader1}
-         ),
-         'This token does not exist'
-     )
+    it('Should Not deposit tokens if token doesnt exist', async () => {
+        await expectRevert(
+            dex.deposit(
+                web3.utils.toWei('100'),
+                web3.utils.fromAscii('FAKE TOKEN'),
+                { from: trader1 }
+            ),
+            'This token does not exist'
+        )
     })
 
-    it('Should Withdraw tokens', async()=>{
-    
+    it('Should Withdraw tokens', async () => {
+
         const amount = web3.utils.toWei('100');
-        await dex.deposit(amount, DAI, {from:trader1});
-        await dex.withdraw(amount, DAI, {from:trader1});
+        await dex.deposit(amount, DAI, { from: trader1 });
+        await dex.withdraw(amount, DAI, { from: trader1 });
 
         const [balanceDex, balanceDai] = await Promise.all([
-            dex.traderBalances(trader1,DAI),
+            dex.traderBalances(trader1, DAI),
             dex.balanceOf(trader1)
         ]);
         assert(balanceDex.isZero());
         assert(balanceDex.toString() === web3.utils.toWei('1000'));
     })
 
-    it('Should NOT withdraw if token does not exist', async()=>{
+    it('Should NOT withdraw if token does not exist', async () => {
         await expectRevert(
             dex.withdraw(
                 web3.utils.toWei('100'),
-                web3.utils.fromAscii('FAKE TOKEN'), 
-                {from: trader1}
+                web3.utils.fromAscii('FAKE TOKEN'),
+                { from: trader1 }
             ),
             'This token does not exist'
         )
     })
 
-    it('Should NOT withdraw if trader balance is less than amount to withdraw (too low)', async()=>{
-        const amount = web3.utils.toWei('100');
-        await dex.deposit(amount, DAI, {from:trader1});
+    // it('should NOT withdraw tokens if balance too low', async () => {
+    //     await dex.deposit(
+    //       web3.utils.toWei('100'),
+    //       DAI,
+    //       {from: trader1}
+    //     );
 
-        await expectRevert(
-            dex.withdraw(
-                web3.utils.toWei('1000'),
-                DAI, 
-                {from: trader1}
-            ),
-            'Balance too low'
-        )
+    //     await expectRevert(
+    //       dex.withdraw(
+    //         web3.utils.toWei('1000'),
+    //         DAI,
+    //         {from: trader1}
+    //       ),
+    //       'balance too low'
+    //     );
+    //   });
+
+    it('should create a limit order', async () => {
+        await dex.deposit(amount, DAI, { from: trader1 });
+        await dex.createLimitOrder(
+            CWV,
+            web3.utils.toWei('10'),
+            10,
+            SIDE.BUY,
+            { from: trader1 }
+        );
+
+        const buyOrders = await dex.getOrders(CWV, SIDE.BUY);
+        const sellOrders = await dex.getOrders(CWV, SIDE.SELL);
+
+        assert(buyOrders.length === 1);
+        assert(buyOrders[0].trader === trader1);
+        assert(buyOrders[0].ticker === web3.utils.padRight(CWV, 64));
+        assert(buyOrders[0].price === "10");
+        assert(buyOrders[0].amount === web3.utils.toWei('10'));
+        assert(sellOrders.length === 0);
     });
-})
+});
