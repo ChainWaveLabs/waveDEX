8399a99e0f67d6c67706ff9cdfecabb3d1a47bc0
Benjamin M. Brown <benjaminbb@gmail.com>
2020-11-01 08:27:31 +0000

    deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,23 +6,62 @@ const Cwv = artifacts.require('mocks/ChainwaveToken.sol');
 const DEX = artifacts.require('ChainwaveDex.sol');
 
 const [DAI, BAT, YFI, CWV] = ['DAI', 'BAT', 'YFI', 'CWV']
-.map(ticker => web3.utils.fromAscii(ticker))
+    .map(ticker => web3.utils.fromAscii(ticker))
 
-module.exports = async function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer, _network, accounts) {
+    const [trader1, trader2, trader3, trader4, _] = accounts;
 
-  await Promise.all(
-      [ Dai, Bat, Yfi,Cwv,DEX].map(contract => deployer.deploy(contract))
-  );
+    await Promise.all(
+        [Dai, Bat, Yfi, Cwv, DEX].map(contract => deployer.deploy(contract))
+    );
 
-  const [dai,bat,yfi,cwv, dex] = await Promise.all(
-      [Dai, Bat, Yfi,Cwv,DEX].map(contract => contract.deployed())
-  );
+    const [dai, bat, yfi, cwv, dex] = await Promise.all(
+        [Dai, Bat, Yfi, Cwv, DEX].map(contract => contract.deployed())
+    );
 
-  await Promise.all([
-    dex.addToken(DAI, dai.address),
-    dex.addToken(BAT, bat.address),
-    dex.addToken(YFI, yfi.address),
-    dex.addToken(CWV, cwv.address),
-])
+    await Promise.all([
+        dex.addToken(DAI, dai.address),
+        dex.addToken(BAT, bat.address),
+        dex.addToken(YFI, yfi.address),
+        dex.addToken(CWV, cwv.address),
+    ])
+
+    const amount = web3.utils.toWei('10000');
+
+    const seedTokenBalance = async (token, trader) => {
+        await token.faucet(trader, amount);
+        await token.approve(dex.address, amount, { from: trader })
+        const ticker = await token.name();
+        await dex.deposit(amount, web3.utils.fromAscii(ticker), { from: trader })
+    }
+
+
+
+    await Promise.all([
+        [dai, bat, yfi, cwv]
+            .map(
+                token => seedTokenBalance(token, trader1)
+            )
+    ])
+
+    await Promise.all([
+        [dai, bat, yfi, cwv]
+            .map(
+                token => seedTokenBalance(token, trader2)
+            )
+    ])
+
+    await Promise.all([
+        [dai, bat, yfi, cwv]
+            .map(
+                token => seedTokenBalance(token, trader3)
+            )
+    ])
+
+    await Promise.all([
+        [dai, bat, yfi, cwv]
+            .map(
+                token => seedTokenBalance(token, trader4)
+            )
+    ])
 };
